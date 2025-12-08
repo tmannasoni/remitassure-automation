ef370872d392cc14a290d3509a32db0c0390c2eb
Ramneek <neek3190@gmail.com>
2025-12-08 18:20:51 +0530

    Updated DashboardPage.java - added methods

--- src/main/java/com/remitassure/pages/DashboardPage.java
+++ src/main/java/com/remitassure/pages/DashboardPage.java
@@ -1,6 +1,11 @@
 package com.remitassure.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import com.remitassure.base.AbstractPage;
 
@@ -8,11 +13,37 @@ public class DashboardPage extends AbstractPage {
 
 	DashboardPage(WebDriver driver) {
 		super(driver);
+		  PageFactory.initElements(driver, this);
 		
 	}
 	
+	@FindBy(xpath = "//a[@class=logactin]")
+	public WebElement loginButton;
+	
+	@FindBy(xpath="//select[contains(@class,'login-code-select')]")
+	public WebElement countryCodeButton;
+	
+	@FindBy(xpath="//select[contains(@class,'login-code-select')]//option")
+	public List<WebElement> countryCodeOptions;
+	
+	@FindBy(css="input[placeholder='Email/Mobile']")
+	public WebElement emailAndMobileInput;
+
+	@FindBy(css="input[placeholder='Enter Password...']")
+	public WebElement passwordField;
+
+	@FindBy(xpath = "//a[@class=login_button]")
+	public WebElement loginSubmit;
+	
 	public void loginClick() {
-	driver.findElement(null)
+	loginButton.click();
+	}
+	
+	public void loginByPhone(String countryCode, int number, String password) {
+		countryCodeButton.click();
+		
+		
+		
 	}
 	
 	
